d34b07f5caa4634b4495bd07c9b83859a5e9ca7c
Tomixbo <tomixbo@gmail.com>
2025-03-10 14:55:47 +0300

    add MariaDB and using env in config

--- app/config.py
+++ app/config.py
@@ -1,8 +1,18 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db")
+    #SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db") #sqlite
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    #SECRET_KEY = "supersecretkey"
