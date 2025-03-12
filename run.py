7bab7426fae43769227b8e3f167b7cc3ae6e3d31
Tomixbo <tomixbo@gmail.com>
2025-03-12 23:09:30 +0300

    add all addresses 0.0.0.0

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from flask_migrate import Migrate
 from app import create_app, db
+from flask_cors import CORS
 
 app = create_app()
-migrate = Migrate(app, db)  # Initialisation de Flask-Migrate
+
+# Initialisation de Flask-Migrate
+migrate = Migrate(app, db)  
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Crée la base de données si elle n'existe pas
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
